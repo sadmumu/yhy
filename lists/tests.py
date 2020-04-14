1f2899bbb467c24f82928c1119ae325146b30ba8
yhy <503624195@qq.com>
2020-04-14 10:43:26 +0800

    Front page HTML now generated from a template

--- lists/tests.py
+++ lists/tests.py
@@ -8,9 +8,7 @@ from lists.views import home_page
 
 class HomePageTest(TestCase):
     
-    def test_root_url_resolve_to_home_page_view(self):
-        found =resolve('/')
-        self.assertEqual(found.func,home_page)
+    
         
     def test_home_page_returns_correct_html(self):
         
